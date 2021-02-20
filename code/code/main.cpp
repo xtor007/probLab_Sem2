208b8511d8decbaf54018cf44308b1e9adb07359
Anatoliy <tolxpams@gmail.com>
2021-02-20 13:49:04 +0200

    new commit

---
+++ code/code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  code
+//
+//  Created by Anatoliy Khramchenko on 2/20/21.
+//  Copyright © 2021 Anatoliy Khramchenko. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
